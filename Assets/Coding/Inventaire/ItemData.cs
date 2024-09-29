d46e26c4ed3e423b3f8f35617dc584ee977b567e
Duxeon <duxeon000@gmail.com>
2024-09-29 20:31:20 +0200

    .,

---
+++ Assets/Coding/Inventaire/ItemData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Items/New item")]
+public class ItemData : ScriptableObject
+{
+    public string _name
+    public Sprite _sprite
+    public GameObject _prefab
+        zihegvyifogczqgfutzgfygfyureyqgvdiudddddbnjbhugyftcgvybhjhugvycftrxygvubhkbihugvyfctxdcfygvbhjugcfyt
+        hfxdychfj
+        nzrynhgfvbr
+        zbtrrrrrrrrrrrrrrrrrrrrrrngsbfff
+        ntrsdgggbnvcfshtrfgnhbtrrgfhbtrgfhgberdtfhbrtfdhgbrtsfhgrzesdfffffffffffffffffff
+}
