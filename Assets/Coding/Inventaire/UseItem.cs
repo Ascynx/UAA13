0378cdbefdb8155b926079a4fa2057a580367a46
Duxeon <duxeon000@gmail.com>
2024-10-10 12:41:48 +0200

    Je continue UseItem

--- Assets/Coding/Inventaire/UseItem.cs
+++ Assets/Coding/Inventaire/UseItem.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,6 +8,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 using static ItemData;
+using static Unity.VisualScripting.Metadata;
 
 public class UseItem : EventTrigger
 {
@@ -14,29 +16,41 @@ public class UseItem : EventTrigger
     public Inventory inventaire;
     public GameObject panelUse;
     public GameObject buttonUse;
+    public GameObject canvas;
 
     private GameObject panel;
     private GameObject btn1;
     private GameObject btn2;
     private GameObject btn3;
     private Transform tmp;
+
+    public void Start()
+    {
+   
+    }
     public override void OnPointerClick(PointerEventData data)
     {
+        foreach (Transform child in canvas.transform)
+        {
+            if (child.name != "Scroll View")
+            {
+                Destroy(child.gameObject);
+            }
+        }
 
-        Debug.Log("0");
         panel = Instantiate(panelUse, transform.position, transform.rotation);
-        panel.transform.SetParent(transform);
+        panel.transform.SetParent(canvas.transform);
+        
+        panel.transform.position = Input.mousePosition;
 
         btn1 = Instantiate(buttonUse, transform.position, transform.rotation);
         tmp = btn1.transform.Find("Text (TMP)");
         tmp.GetComponent<TextMeshProUGUI>().text = "Info";
         btn1.transform.SetParent(panel.transform);
 
-        Debug.Log("1");
         switch (item.type)  {
             case (classe.Sword):
 
-                Debug.Log("2");
                 btn2 = Instantiate(buttonUse, transform.position, transform.rotation);
                 tmp = btn1.transform.Find("Text (TMP)");
                 tmp.GetComponent<TextMeshProUGUI>().text = "Équiper l'épée";
