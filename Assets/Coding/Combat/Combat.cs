d3bed5d29a876e5e6574792796dfea52af4ad465
Duxeon <duxeon000@gmail.com>
2024-10-17 13:58:22 +0200

    Fin code de combat purement de type code

--- Assets/Coding/Combat/Combat.cs
+++ Assets/Coding/Combat/Combat.cs
@@ -4,22 +4,100 @@ using UnityEngine;
 
 public class Combat : MonoBehaviour
 {
-    mob ennemi;
-    mob player;
-
-    Attaque attackEnnemi;
-    Attaque attackPlayer;
-    void turn()
+    
+    void turn(mob ennemi, mob player, Attaque attackEnnemi, Attaque attackPlayer)
     {
-        int precisionPlayer = attackPlayer.precision;
-        if (player.effet == mob.effect.LowerPrecision)
+        attaque(attackPlayer, player, ref ennemi);
+        if (ennemi.pvactuel > 0)
         {
-            precisionPlayer = int.Parse(Mathf.Round(float.Parse(precisionPlayer.ToString()) / 2).ToString);
+            apliqueEffect(attackPlayer.effet, ref ennemi);
+            attaque(attackEnnemi, ennemi, ref player);
+            if (player.pvactuel > 0)
+            {
+                apliqueEffect(attackEnnemi.effet, ref player);
+            }
         }
-        if (Random.Range(0, 100) <= attackPlayer.precision)
+    }
+
+    void apliqueEffect(Attaque.effect effet, ref mob victime)
+    {
+        bool isnotboss = (victime.Type1 != mob.type.Boss || victime.Type2 != mob.type.Boss);
+        bool isnotsemboss = (victime.Type1 != mob.type.Boss || victime.Type2 != mob.type.Boss) && (victime.Type1 != mob.type.SemiBoss || victime.Type2 != mob.type.SemiBoss);
+        if (effet == Attaque.effect.None || victime.effet != mob.effect.None)
         {
 
         }
-        ennemi.pvactuel -= attackPlayer.power;
+        if (effet == Attaque.effect.Paralize)
+        {
+            if (Random.Range(0, 10) == 0 && (victime.Type1 != mob.type.Elec || victime.Type2 != mob.type.Elec) && isnotboss)
+            {
+                victime.effet = mob.effect.Paralize;
+            }
+        } 
+        else if (effet == Attaque.effect.Burn)
+        {
+            if (Random.Range(0, 10) == 0 && (victime.Type1 != mob.type.Fire || victime.Type2 != mob.type.Fire) && isnotboss)
+            {
+                victime.effet = mob.effect.Burn;
+            }
+        }
+        else if(effet == Attaque.effect.Toxic && isnotboss)
+        {
+            if (Random.Range(0, 10) == 0)
+            {
+                victime.effet = mob.effect.Toxic;
+            }
+        }
+        else if (effet == Attaque.effect.PowerWind)
+        {
+            if ((victime.Type1 == mob.type.Aerial || victime.Type2 == mob.type.Aerial) && isnotsemboss)
+            {
+                victime.pvactuel = 0;
+            }
+        }
+        else if (effet == Attaque.effect.LowerPrecision)
+        {
+            if (Random.Range(0, 10) == 0 && isnotsemboss)
+            {
+                victime.effet = mob.effect.LowerPrecision;
+            }
+        }
+        else if (effet == Attaque.effect.LowerPower)
+        {
+            if (Random.Range(0, 10) == 0 && isnotsemboss)
+            {
+                victime.effet = mob.effect.LowerPower;
+            }
+        }
+        else if (effet == Attaque.effect.LowerDef)
+        {
+            if (Random.Range(0, 10) == 0 && isnotsemboss)
+            {
+                victime.effet = mob.effect.LowerDef;
+            }
+        }
+    }
+    void attaque(Attaque att, mob attaquant, ref mob victime)
+    {
+        int precision = att.precision;
+        int power = att.power;
+        if (attaquant.effet == mob.effect.LowerPrecision)
+        {
+            precision = precision / 2;
+        } else if (attaquant.effet == mob.effect.LowerPower)
+        {
+            power = power / 2;
+        }
+        else if (victime.effet == mob.effect.LowerDef)
+        {
+            power = power * 2;
+        }
+        if (Random.Range(0, 100) <= precision)
+        {
+            if (attaquant.effet != mob.effect.Paralize || Random.Range(0, 1) == 0)
+            {
+                victime.pvactuel -= power;
+            }
+        }
     }
 }
