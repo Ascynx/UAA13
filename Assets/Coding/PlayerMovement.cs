8a4d03b9a0d1d8c1c0f26a640d1b799a08c11c92
Vandervoort Alexandre <220857@CCM-Asty-Moulin.local>
2024-11-28 14:21:39 +0100

    fin smooth movement

--- Assets/Coding/PlayerMovement.cs
+++ Assets/Coding/PlayerMovement.cs
@@ -1,95 +1,75 @@
+using System;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float vitesse;
     public float baseVitesse;
     public Rigidbody2D ceci;
-    Vector3 deplacement;
     bool inventaireIsOuvert;
     public GameObject inventaire;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    public Vector2 startMarker;
-    public Vector2 endMarker;
+    public float accelerationDirection = 3F;
 
-    private float movementSpeed = 1.0F;
-    private readonly int framesForMovement = 45;
-    private float movementStartedFramesAgo = -1;
+    public void OnDirection(InputAction.CallbackContext valeur)
+    {
+
+    }
 
     void Update()
     {
         if (!inventaire.activeInHierarchy)
         {
-            bool inMovement = false;
-            if (movementStartedFramesAgo != -1)
+            Vector2 velocity = new Vector2(0.0F, 0.0F);
+            if (Keybinds.Up.isPressed)
             {
-                float ratioCovered = (movementStartedFramesAgo / (framesForMovement / movementSpeed));
-                Vector2 lerpPosition = Vector2.Lerp(startMarker, endMarker, ratioCovered);
-                ceci.position = lerpPosition;
-                movementStartedFramesAgo += 1;
-                if (ratioCovered >= 1)
-                {
-                    movementStartedFramesAgo = -1;
-                }
-                if (ratioCovered > -1)
-                {
-                    inMovement = true;
-                }
+                velocity += new Vector2(0, 1);
+            }
+            if (Keybinds.Down.isPressed)
+            {
+                velocity += new Vector2(0, -1);
             }
 
-
-
-            if (!inMovement)
+            if (Keybinds.Right.isPressed)
             {
-                Vector2 velocity;
-                vitesse = baseVitesse;
-                if (Keybinds.Courir.isPressed)
-                {
-                    vitesse += baseVitesse;
-                }
-                if (Keybinds.Up.isPressed)
-                {
-                    deplacement = new Vector3(0, 1, 0);
-                    velocity = deplacement;
-                }
-                else if (Keybinds.Down.isPressed)
-                {
-                    deplacement = new Vector3(0, -1, 0);
-                    velocity = deplacement;
-                }
-                else if (Keybinds.Right.isPressed)
-                {
-                    deplacement = new Vector3(1, 0, 0);
-                    velocity = deplacement;
-                }
-                else if (Keybinds.Left.isPressed)
-                {
-                    deplacement = new Vector3(-1, 0, 0);
-                    velocity = deplacement;
-                }
-                else velocity = new Vector3(0, 0, 0);
-
-                transform.eulerAngles = new Vector3(
-                    0,
-                    0,
-                    0
-                );
-
-                
-                if (velocity != new Vector2(0, 0))
-                {
-                    movementSpeed = vitesse / baseVitesse;
-                    startMarker = ceci.position;
-                    endMarker = startMarker + velocity;
-                    movementStartedFramesAgo = 0;
-                }
-                
+                velocity += new Vector2(1, 0);
             }
+            if (Keybinds.Left.isPressed)
+            {
+                velocity += new Vector2(-1, 0);
+            }
+
+            float speed = baseVitesse * (Keybinds.Courir.isPressed ? 2 : 1);
+            if (velocity.magnitude > 1) velocity.Normalize();
+            Vector2 targetVelocity = velocity * speed;
+            Vector2 newVelocity = Vector2.MoveTowards(ceci.velocity, targetVelocity, Time.deltaTime * accelerationDirection);
+            
+            ceci.velocity = newVelocity;
+            transform.eulerAngles = new Vector3(0, 0, 0);
         }
+    }
+
+
+    [SerializeField][Range(0.5F, 2)] private float arrowLength = 1.0F;
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+
+        var position = transform.position;
+        var velocity = ceci.velocity;
+
+        if (velocity.magnitude < 0.1f) return;
+        if (velocity.magnitude > 1)
+        {
+            velocity.Normalize();
+        }
+
+        Handles.color = Color.red;
+        Handles.ArrowHandleCap(0, position, Quaternion.LookRotation(new Vector3(velocity.x, velocity.y, 0)), arrowLength, EventType.Repaint);
     }
 }
